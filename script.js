80caed8920e5dc144799531973a689de71940afb
kleintrevor <trevor.klein@gmail.com>
2022-08-04 04:23:24 -0500

    adding on click function for drawImage

---
+++ script.js
@@ -0,0 +1,29 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = 500;
+canvas.height = 700;
+const explosions = [];
+
+class Explosions {
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+        this.spriteWidth = 200;
+        this.spriteHeight = 179;
+        this.width = this.spriteWidth/2;
+        this.height = this.spriteHeight/2;
+        this.image = new Image();
+        this.image.src = 'boom.png';
+        this.frame = 0;
+    }
+    update(){
+        this.frame++;
+    }
+    draw(){
+        // ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh)
+        ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+    }
+}
+window.addEventListener('click', function(e){
+    ctx.fillRect(x, y, width, height);
+})
